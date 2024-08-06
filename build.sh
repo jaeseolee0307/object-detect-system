b72a265511b83483c6f48f94be29b9ee9c274ac4
jaeseolee0307 <jaeseolee0307@gmail.com>
2024-08-07 00:07:47 +0900

    add poky and meta-raspberrypi

---
+++ build.sh
@@ -0,0 +1,35 @@
+#!/bin/bash
+
+git submodule init
+git submodule sync
+git submodule update
+
+# local.conf won't exist until this step on first execution
+source poky/oe-init-build-env
+
+CONFLINE="MACHINE = \"raspberypi5\""
+
+cat conf/local.conf | grep "${CONFLINE}" > /dev/null
+local_conf_info=$?
+
+if [ $local_conf_info -ne 0 ];then
+	echo "Append ${CONFLINE} in the local.conf file"
+	echo ${CONFLINE} >> conf/local.conf
+	
+else
+	echo "${CONFLINE} already exists in the local.conf file"
+fi
+
+
+bitbake-layers show-layers | grep "meta-raspberrypi" > /dev/null
+layer_info=$?
+
+if [ $layer_info -ne 0 ];then
+	echo "Adding meta-raspberrypi layer"
+	bitbake-layers add-layer ../meta-raspberrypi
+else
+	echo "meta-raspberrypi layer already exists"
+fi
+
+set -e
+bitbake core-image-minimal
